2b65c99a5aba2ab3a6e3a9e51380dfd9f89e15c1
Manojjahgirdar <jahgirdarmanoj@gmail.com>
2020-02-12 15:20:56 +0530

    changes in code

--- AvatarChatbotApp/app/src/main/java/com/ibm/avatarchatbot/ClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.avatarchatbot;
-
-import android.view.View;
-
-public interface ClickListener {
-    void onClick(View view, int position);
-
-    void onLongClick(View view, int position);
-}
