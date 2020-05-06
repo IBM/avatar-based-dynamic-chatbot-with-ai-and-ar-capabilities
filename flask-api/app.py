14dd5ab3689bcceba033c0d55885e418a4ed5717
AffineR <peterinwestend@gmail.com>
2020-05-06 06:28:42 +0100

    Update app.py

--- flask-api/app.py
+++ flask-api/app.py
@@ -32,7 +32,7 @@ def getRestaurants(coordinates):
     information_about_restaurant = []
 
     getRestaurantsFromLatAndLon = 'https://developers.zomato.com/api/v2.1/search?lat='+str(lat)+'&lon='+str(lon)+ '&radius=500&sort=real_distance&order=asc&start=0&count=20'
-    header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user-key": ""}
+    header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user-key": "2874040a2f5a7bbe4e13b5b6b21cedd9"}
     response = requests.get(getRestaurantsFromLatAndLon, headers=header)
     Restaurant_info = response.json()
    
